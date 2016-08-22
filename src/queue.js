ecdf8ade26657d32f92512b51d42b5e24852dc12
Andrei Levkov <levkov.andrew@gmail.com>
2016-08-22 14:43:30 +0300

    PriorityQueue class

--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,32 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap;
 	}
 
 	push(data, priority) {
-
+		if (this.size() < this.maxSize) {
+			this.heap.push(data, priority);
+		} else {
+			throw "queue has max size";
+		};
 	}
 
 	shift() {
-
+		if (!this.isEmpty()) {
+			return this.heap.pop();
+		} else {
+			throw "queue is empty";
+		};
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
